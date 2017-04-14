36ffd692abcca3b4b7ce6c1978e97a9efe49acf1
Abby Sepple <abby.sepple@gmail.com>
2017-04-14 17:13:17 -0500

    This is the Final Countdown

--- pro_practice/script.js
+++ pro_practice/script.js
@@ -8,3 +8,14 @@
   BONUS 1: Add a button for the user to reset the timer.
   BONUS 2: Make something cool happen using a jQuery animation when the timer gets to zero.
 */
+
+//the final countdown!
+var sec = 10;
+var timer = setInterval(function() {
+   $('.count').text(sec--);
+   if (sec === -1) {
+      $('.count').fadeOut('slow');
+      clearInterval(timer);
+   }
+}, 1000);
+//end final cuntdown
